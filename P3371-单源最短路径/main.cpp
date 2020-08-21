5ce1f45fca2c6ba561b46708ec9e3221883c7ed0
byx2000 <416982860@qq.com>
2020-08-21 17:54:55 +0800

    添加题目：P3371-单源最短路径

---
+++ P3371-单源最短路径/main.cpp
@@ -0,0 +1,165 @@
+// https://www.luogu.com.cn/problem/P3371
+// https://www.luogu.com.cn/problem/P4779
+
+#include <iostream>
+#include <vector>
+#include <climits>
+#include <queue>
+
+using namespace std;
+
+#define INF (INT_MAX / 2)
+
+struct Edge
+{
+	int to, distance;
+	Edge(int to, int distance) : to(to), distance(distance) {}
+};
+
+// 解法1：分支限界法（普通队列）
+class Solution1
+{
+public:
+	Solution1(const vector<vector<Edge>>& edges, int start)
+		: edges(edges), start(start) {}
+
+	vector<int> solve()
+	{
+		int cNode = edges.size();
+
+		vector<int> shortest(cNode);
+		for (int i = 0; i < cNode; ++i)
+		{
+			shortest[i] = INF;
+		}
+
+		queue<Node> q;
+		q.push(Node(start, 0));
+
+		while (!q.empty())
+		{
+			Node node = q.front();
+			int iNode = node.iNode;
+			int distance = node.distance;
+
+			q.pop();
+
+			if (distance >= shortest[iNode])
+			{
+				continue;
+			}
+
+			shortest[iNode] = distance;
+
+			for (int i = 0; i < (int)edges[iNode].size(); ++i)
+			{
+				int to = edges[iNode][i].to;
+				int dis = edges[iNode][i].distance;
+				if (distance + dis < shortest[to])
+				{
+					q.push(Node(to, distance + dis));
+				}
+			}
+		}
+
+		return shortest;
+	}
+
+private:
+	vector<vector<Edge>> edges;
+	int start;
+
+	struct Node
+	{
+		int iNode;
+		int distance;
+		Node(int iNode, int dis) : iNode(iNode), distance(dis) {}
+		bool operator>(const Node& n) const { return distance > n.distance; }
+	};
+};
+
+// 解法2：分支限界法（优先队列）
+class Solution2
+{
+public:
+	Solution2(const vector<vector<Edge>>& edges, int start)
+		: edges(edges), start(start) {}
+
+	vector<int> solve()
+	{
+		int cNode = edges.size();
+
+		vector<int> shortest(cNode);
+		for (int i = 0; i < cNode; ++i)
+		{
+			shortest[i] = INF;
+		}
+
+		priority_queue<Node, vector<Node>, greater<Node>> pq;
+		pq.push(Node(start, 0));
+
+		while (!pq.empty())
+		{
+			Node node = pq.top();
+			int iNode = node.iNode;
+			int distance = node.distance;
+
+			pq.pop();
+
+			if (distance >= shortest[iNode])
+			{
+				continue;
+			}
+
+			shortest[iNode] = distance;
+
+			for (int i = 0; i < (int)edges[iNode].size(); ++i)
+			{
+				int to = edges[iNode][i].to;
+				int dis = edges[iNode][i].distance;
+				if (distance + dis < shortest[to])
+				{
+					pq.push(Node(to, distance + dis));
+				}
+			}
+		}
+
+		return shortest;
+	}
+
+private:
+	vector<vector<Edge>> edges;
+	int start;
+
+	struct Node
+	{
+		int iNode;
+		int distance;
+		Node(int iNode, int dis) : iNode(iNode), distance(dis) {}
+		bool operator>(const Node& n) const { return distance > n.distance; }
+	};
+};
+
+int main()
+{
+	int cNode, cEdge, start;
+	cin >> cNode >> cEdge >> start;
+
+	vector<vector<Edge>> edges(cNode);
+	for (int i = 0; i < cEdge; ++i)
+	{
+		int from, to, distance;
+		cin >> from >> to >> distance;
+		edges[from - 1].push_back(Edge(to - 1, distance));
+	}
+
+	//vector<int> res = Solution1(edges, start - 1).solve();
+	vector<int> res = Solution2(edges, start - 1).solve();
+
+	for (int i = 0; i < cNode; ++i)
+	{
+		cout << (res[i] == INF ? INT_MAX : res[i]) << " ";
+	}
+
+	return 0;
+}
