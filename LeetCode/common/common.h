3061f03a8c69afd6d4c615daef79101d11f9e245
byx2000 <416982860@qq.com>
2020-09-05 21:45:09 +0800

    向common.h中添加单元测试工具

--- LeetCode/common/common.h
+++ LeetCode/common/common.h
@@ -1,3 +1,9 @@
+/*
+* LeetCode题解公共头文件
+* 包含C++标准库常用头文件，以及单元测试工具
+* 单元测试工具用于LeetCode题目样例的测试
+*/
+
 #ifndef COMMON_H
 #define COMMON_H
 
@@ -13,5 +19,56 @@
 #include <cassert>
 
 using namespace std;
+
+// 断言
+class Assert
+{
+public:
+    // 是否为真
+    // 适用于所有能够转换成bool的类型
+    template<typename T>
+    static bool isTrue(const T& condition)
+    {
+        return condition;
+    }
+
+    // 是否相等
+    // 适用于所有可以用==比较的类型
+    template<typename T1, typename T2>
+    static bool areEqual(const T1& a, const T2& b)
+    {
+        return a == b;
+    }
+
+    // 判断集合是否相等
+    // 适用于C++容器类：vector、string等
+    // 集合相等意味着两集合所含元素相等，不考虑元素顺序
+    template<typename T>
+    static bool areEquivalent(const T& a, const T& b)
+    {
+        if (size(a) != size(b))
+        {
+            return false;
+        }
+        return set<T>(begin(a), end(a)) == set<T>(begin(b), end(b));
+    }
+
+    // 判断集合是否相等
+    // 适用于传统C风格数组
+    // 集合相等意味着两集合所含元素相等，不考虑元素顺序
+    template<typename T, int Size1, int Size2>
+    static bool areEquivalent(T(&a)[Size1], T(&b)[Size2])
+    {
+        if (Size1 != Size2)
+        {
+            return false;
+        }
+        return set<T>(begin(a), end(a)) == set<T>(begin(b), end(b));
+    }
+};
+
+// 单元测试
+// 当条件condition为假时，程序退出，并打印出错行号
+#define TEST(condition) do{if(!(condition))assert(false);}while(0)
 
 #endif
---
+++ LeetCode/common/main.cpp
@@ -0,0 +1,33 @@
+#include "common.h"
+
+int main()
+{
+	TEST(Assert::isTrue(true));
+	TEST(!Assert::isTrue(false));
+
+	TEST(Assert::areEqual(-3, -3));
+	TEST(!Assert::areEqual(-3, 2));
+	TEST(Assert::areEqual(3.14, 3.14));
+	TEST(!Assert::areEqual(3.14, 1.57));
+	TEST(Assert::areEqual(vector<int>{1, 2, 3}, vector<int>{1, 2, 3}));
+	TEST(!Assert::areEqual(vector<int>{1, 2, 3}, vector<int>{1, 2, 3, 4}));
+	TEST(!Assert::areEqual(vector<int>{1, 2, 3}, vector<int>{3, 2, 1}));
+	TEST(Assert::areEqual(string("abc"), "abc"));
+	TEST(!Assert::areEqual("123", string("1234")));
+
+	TEST(Assert::areEquivalent(vector<int>{3, 1, 4, 2}, vector<int>{1, 2, 3, 4}));
+	TEST(!Assert::areEquivalent(vector<int>{3, 1, 4, 2}, vector<int>{2, 1, 4, 5}));
+	TEST(!Assert::areEquivalent(vector<int>{3, 3, 3}, vector<int>{3, 3}));
+	{
+		int a[] = { 1, 2, 3, 4, 5 };
+		int b[] = { 5, 4, 3, 1, 2 };
+		TEST(Assert::areEquivalent(a, b));
+	}
+	{
+		int a[] = { 3, 3, 3 };
+		int b[] = { 3, 3 };
+		TEST(!Assert::areEquivalent(a, b));
+	}
+
+	return 0;
+}
