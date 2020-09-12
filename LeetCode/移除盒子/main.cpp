3ee81757baed96ac0e3c2e43a2f7962a70f80196
byx2000 <416982860@qq.com>
2020-09-12 10:56:20 +0800

    添加题目：LeetCode-移除盒子

---
+++ LeetCode/移除盒子/main.cpp
@@ -0,0 +1,20 @@
+// https://leetcode-cn.com/problems/remove-boxes/
+
+#include "../common/common.h"
+
+class Solution 
+{
+public:
+    int removeBoxes(vector<int>& nums) 
+    {
+
+    }
+};
+
+int main()
+{
+    vector<int> nums{ 1,3,2,2,2,3,4,3,1 };
+    TEST(Assert::areEqual(Solution().removeBoxes(nums), 23));
+
+	return 0;
+}
