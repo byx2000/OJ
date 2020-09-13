199f30749de357f21101c13247615a45f113f42e
byx2000 <416982860@qq.com>
2020-09-13 18:49:21 +0800

    添加题目：LeetCode-让字符串成为回文串的最少插入次数

--- LeetCode/让字符串成为回文串的最少插入次数/main.cpp
+++ LeetCode/让字符串成为回文串的最少插入次数/main.cpp
@@ -2,12 +2,103 @@
 
 #include "../common/common.h"
 
+// 转化为最长回文子序列
+class Solution1
+{
+public:
+    Solution1(const string& s) : s(s) {}
+
+    int solve()
+    {
+        cache = vector<vector<int>>(s.size(), vector<int>(s.size(), -1));
+        return s.size() - dp(0, s.size() - 1);
+    }
+
+private:
+    string s;
+    vector<vector<int>> cache;
+
+    // s[i]~s[j]的最长回文子序列长度
+    int dp(int i, int j)
+    {
+        if (i > j)
+        {
+            return 0;
+        }
+
+        if (cache[i][j] != -1)
+        {
+            return cache[i][j];
+        }
+
+        if (i == j)
+        {
+            return cache[i][j] = 1;
+        }
+
+        if (s[i] == s[j])
+        {
+            return cache[i][j] = 2 + dp(i + 1, j - 1);
+        }
+        else
+        {
+            return cache[i][j] = max(dp(i + 1, j), dp(i, j - 1));
+        }
+    }
+};
+
+// 直接动态规划
+class Solution2
+{
+public:
+    Solution2(const string& s) : s(s) {}
+
+    int solve()
+    {
+        cache = vector<vector<int>>(s.size(), vector<int>(s.size(), -1));
+        return dp(0, s.size() - 1);
+    }
+
+private:
+    string s;
+    vector<vector<int>> cache;
+
+    // 将子串s[i]~s[j]变成回文串的最少插入次数
+    int dp(int i, int j)
+    {
+        if (i > j)
+        {
+            return 0;
+        }
+
+        if (cache[i][j] != -1)
+        {
+            return cache[i][j];
+        }
+
+        if (i == j)
+        {
+            return cache[i][j] = 0;
+        }
+
+        if (s[i] == s[j])
+        {
+            return cache[i][j] = dp(i + 1, j - 1);
+        }
+        else
+        {
+            return cache[i][j] = min(dp(i + 1, j), dp(i, j - 1)) + 1;
+        }
+    }
+};
+
 class Solution 
 {
 public:
     int minInsertions(string s) 
     {
-        return 0;
+        //return Solution1(s).solve();
+        return Solution2(s).solve();
     }
 };
 
