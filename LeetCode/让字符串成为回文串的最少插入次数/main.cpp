4e828e675105fcdfaa5b5a494045185ef8ecf0d8
byx2000 <416982860@qq.com>
2020-09-13 10:55:09 +0800

    添加题目：LeetCode-让字符串成为回文串的最少插入次数

---
+++ LeetCode/让字符串成为回文串的最少插入次数/main.cpp
@@ -0,0 +1,23 @@
+// https://leetcode-cn.com/problems/minimum-insertion-steps-to-make-a-string-palindrome/
+
+#include "../common/common.h"
+
+class Solution 
+{
+public:
+    int minInsertions(string s) 
+    {
+
+    }
+};
+
+int main()
+{
+    TEST(Assert::areEqual(Solution().minInsertions("g"), 0));
+    TEST(Assert::areEqual(Solution().minInsertions("no"), 1));
+    TEST(Assert::areEqual(Solution().minInsertions("zzazz"), 0));
+    TEST(Assert::areEqual(Solution().minInsertions("mbadm"), 2));
+    TEST(Assert::areEqual(Solution().minInsertions("leetcode"), 5));
+
+	return 0;
+}
