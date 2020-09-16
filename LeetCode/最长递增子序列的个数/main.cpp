bfd2b6ee975fbb75d1fb561f806c24003e085dbc
byx2000 <416982860@qq.com>
2020-09-16 19:09:43 +0800

    添加题目：LeetCode-最长递增子序列的个数

---
+++ LeetCode/最长递增子序列的个数/main.cpp
@@ -0,0 +1,72 @@
+// https://leetcode-cn.com/problems/number-of-longest-increasing-subsequence/
+
+#include "../common/common.h"
+
+class Solution 
+{
+public:
+    int findNumberOfLIS(vector<int>& nums) 
+    {
+        if (nums.size() == 0)
+        {
+            return 0;
+        }
+
+        vector<int> length(nums.size()), count(nums.size());
+
+        length[0] = count[0] = 1;
+        int maxLength = 1;
+        for (int i = 1; i < (int)nums.size(); ++i)
+        {
+            length[i] = 1;
+            count[i] = 1;
+            for (int j = 0; j < i; ++j)
+            {
+                if (nums[i] > nums[j])
+                {
+                    if (length[j] + 1 > length[i])
+                    {
+                        length[i] = length[j] + 1;
+                        count[i] = count[j];
+                    }
+                    else if (length[j] + 1 == length[i])
+                    {
+                        count[i] += count[j];
+                    }
+                }
+            }
+            maxLength = max(maxLength, length[i]);
+        }
+
+        int result = 0;
+        for (int i = 0; i < (int)nums.size(); ++i)
+        {
+            if (length[i] == maxLength)
+            {
+                result += count[i];
+            }
+        }
+
+        return result;
+    }
+};
+
+int main()
+{
+    vector<int> nums{ 1,3,5,4,7 };
+    TEST(Assert::areEqual(Solution().findNumberOfLIS(nums), 2));
+
+    nums = vector<int>{ 2,2,2,2,2 };
+    TEST(Assert::areEqual(Solution().findNumberOfLIS(nums), 5));
+
+    nums = vector<int>{ 1,2,4,3,5,4,7,2 };
+    TEST(Assert::areEqual(Solution().findNumberOfLIS(nums), 3));
+
+    nums = vector<int>{  };
+    TEST(Assert::areEqual(Solution().findNumberOfLIS(nums), 0));
+
+    nums = vector<int>{ 1 };
+    TEST(Assert::areEqual(Solution().findNumberOfLIS(nums), 1));
+
+	return 0;
+}
