b0e7f38b4f662173853d1b308bec8857543f54ed
byx2000 <416982860@qq.com>
2020-10-05 20:08:37 +0800

    添加题目：LeetCode-二叉树的前序遍历

--- LeetCode/二叉树的前序遍历/main.cpp
+++ LeetCode/二叉树的前序遍历/main.cpp
@@ -2,12 +2,87 @@
 
 #include "../common/common.h"
 
+// µÝ¹é
+class Solution1
+{
+public:
+    Solution1(TreeNode* root) : root(root) {}
+
+    vector<int> solve()
+    {
+        result.clear();
+        traverse(root);
+        return result;
+    }
+
+private:
+    TreeNode* root;
+    vector<int> result;
+
+    void traverse(TreeNode* root)
+    {
+        if (root == NULL) return;
+        result.push_back(root->val);
+        traverse(root->left);
+        traverse(root->right);
+    }
+};
+
+// ·ÇµÝ¹é
+class Solution2
+{
+public:
+    Solution2(TreeNode* root) : root(root) {}
+
+    vector<int> solve()
+    {
+        vector<int> result;
+        stack<State> callStack;
+        callStack.push(State(root, 0));
+
+        while (!callStack.empty())
+        {
+            State& cur = callStack.top();
+
+            if (cur.s == 0)
+            {
+                if (cur.p == NULL) { callStack.pop(); continue; }
+                result.push_back(cur.p->val);
+                cur.s = 1;
+                callStack.push(State(cur.p->left, 0));
+            }
+            else if (cur.s == 1)
+            {
+                cur.s = 2;
+                callStack.push(State(cur.p->right, 0));
+            }
+            else
+            {
+                callStack.pop();
+            }
+        }
+
+        return result;
+    }
+
+private:
+    TreeNode* root;
+
+    struct State
+    {
+        TreeNode* p;
+        int s;
+        State(TreeNode* p, int s) : p(p), s(s) {}
+    };
+};
+
 class Solution 
 {
 public:
     vector<int> preorderTraversal(TreeNode* root) 
     {
-        return vector<int>();
+        //return Solution1(root).solve();
+        return Solution2(root).solve();
     }
 };
 
