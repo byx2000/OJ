ae3f1da62957bfdd386ac3a14741fab696d14012
byx2000 <416982860@qq.com>
2020-10-06 11:03:55 +0800

    添加题目：LeetCode-二叉树的深度

---
+++ LeetCode/二叉树的深度/main.cpp
@@ -0,0 +1,100 @@
+// https://leetcode-cn.com/problems/er-cha-shu-de-shen-du-lcof/
+
+#include "../common/common.h"
+
+// µÝ¹é
+class Solution1
+{
+public:
+    Solution1(TreeNode* root) : root(root) {}
+
+    int solve()
+    {
+        return depth(root);
+    }
+
+private:
+    TreeNode* root;
+
+    int depth(TreeNode* root)
+    {
+        if (root == NULL) return 0;
+        return 1 + max(depth(root->left), depth(root->right));
+    }
+};
+
+// ·ÇµÝ¹é
+class Solution2
+{
+public:
+    Solution2(TreeNode* root) : root(root) {}
+
+    int solve()
+    {
+        stack<State> callStack;
+        callStack.push(State(root, 0));
+        int retVal = 0;
+
+        while (!callStack.empty())
+        {
+            State& cur = callStack.top();
+
+            if (cur.s == 0)
+            {
+                if (cur.p == NULL) 
+                { 
+                    retVal = 0; callStack.pop(); continue; 
+                }
+                cur.s = 1;
+                callStack.push(State(cur.p->left, 0));
+            }
+            else if (cur.s == 1)
+            {
+                cur.t = retVal;
+                cur.s = 2;
+                callStack.push(State(cur.p->right, 0));
+            }
+            else if (cur.s == 2)
+            {
+                cur.t = max(cur.t, retVal) + 1;
+                retVal = cur.t;
+                callStack.pop();
+            }
+        }
+
+        return retVal;
+    }
+
+private:
+    TreeNode* root;
+
+    struct State
+    {
+        TreeNode* p;
+        int s;
+        int t;
+        State(TreeNode* p, int s) : p(p), s(s), t(0) {}
+    };
+};
+
+class Solution 
+{
+public:
+    int maxDepth(TreeNode* root) 
+    {
+        //return Solution1(root).solve();
+        return Solution2(root).solve();
+    }
+};
+
+int main()
+{
+    Cases<TreeNode*, int> cases
+    {
+        {BuildTree({3,9,20,null,null,15,7}), 3},
+    };
+
+    test(&Solution::maxDepth, cases);
+
+	return 0;
+}
