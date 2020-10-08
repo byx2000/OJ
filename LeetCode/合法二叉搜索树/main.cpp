767017627cb41960376b22f106c42860216f1004
byx2000 <416982860@qq.com>
2020-10-08 20:11:11 +0800

    修改题目：LeetCode-合法二叉搜索树

--- LeetCode/合法二叉搜索树/main.cpp
+++ LeetCode/合法二叉搜索树/main.cpp
@@ -35,12 +35,58 @@ private:
     }
 };
 
+// ·ÇµÝ¹é
+class Solution2
+{
+public:
+    Solution2(TreeNode* root) : root(root) {}
+
+    bool solve()
+    {
+        vector<int> nodes;
+        stack<Frame> callStack;
+        callStack.push(Frame(root, 0));
+
+        while (!callStack.empty())
+        {
+            Frame& cur = callStack.top();
+
+            if (cur.s == 0)
+            {
+                if (cur.p == NULL) { callStack.pop(); continue; }
+                callStack.push(Frame(cur.p->left, 0));
+                cur.s = 1;
+            }
+            else if (cur.s == 1)
+            {
+                if (nodes.size() > 0 && nodes.back() >= cur.p->val) return false;
+                nodes.push_back(cur.p->val);
+                callStack.pop();
+                callStack.push(Frame(cur.p->right, 0));
+            }
+        }
+
+        return true;
+    }
+
+private:
+    TreeNode* root;
+
+    struct Frame
+    {
+        TreeNode* p;
+        int s;
+        Frame(TreeNode* p, int s) : p(p), s(s) {}
+    };
+};
+
 class Solution 
 {
 public:
     bool isValidBST(TreeNode* root) 
     {
-        return Solution1(root).solve();
+        //return Solution1(root).solve();
+        return Solution2(root).solve();
     }
 };
 
