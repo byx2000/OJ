92d1554650b847e70b1b46f888e16a723bdf803f
byx <18023699313@163.com>
2021-01-15 20:31:03 +0800

    添加题目：LeetCode-x的平方根

---
+++ LeetCode/x的平方根/main.cpp
@@ -0,0 +1,35 @@
+#include "../common/common.h"
+
+class Solution 
+{
+public:
+	int mySqrt(int x) 
+	{
+		int left = 0, right = x;
+		while (right - left > 3)
+		{
+			long long mid = left + (right - left) / 2;
+			if (mid * mid == x) return mid;
+			else if (mid * mid < x) left = mid;
+			else right = mid - 1;
+		}
+
+		for (int i = left; i <= right; ++i)
+		{
+			if (i * i == x) return i;
+			else if (i * i > x) return i - 1;
+		}
+
+		return right;
+	}
+};
+
+int main()
+{
+	TEST(Assert::areEqual(Solution().mySqrt(0), 0));
+	TEST(Assert::areEqual(Solution().mySqrt(1), 1));
+	TEST(Assert::areEqual(Solution().mySqrt(4), 2));
+	TEST(Assert::areEqual(Solution().mySqrt(6), 2));
+	TEST(Assert::areEqual(Solution().mySqrt(8), 2));
+	return 0;
+}
